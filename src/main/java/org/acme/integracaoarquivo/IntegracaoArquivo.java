35c65e89b01476917b32da883f2a0a5113adf178
Alexandre Bezerra de Andrade <solidwinter96@outlook.com>
2023-10-29 23:55:28 -0300

    configurando headers

--- src/main/java/org/acme/integracaoarquivo/IntegracaoArquivo.java
+++ src/main/java/org/acme/integracaoarquivo/IntegracaoArquivo.java
@@ -1,15 +1,20 @@
 package org.acme.integracaoarquivo;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.file.FileConstants;
 
 public class IntegracaoArquivo extends RouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
 		
-		from("file:/tmp/apache_camel/entrada/")
+		from("file:{{diretorioEntrada}}?delay=5000")
 		.routeId("Integracao-Arquivo")
-		.to("file:/tmp/apache_camel/saida/");
+		.log("Processando o arquivo: ${file:onlyname}")
+		//setando o nome do arquivo como (Hora atual)_(Nome do arquivo)
+		.setHeader(FileConstants.FILE_NAME, simple("${date:now:HHmmss}_${file:name}")) // setando nome na entrada
+		// .to("file:{{diretorioSaida}}?fileName=${date:now:HHmmss}_${file:name}") //setando nome na saida
+		.to("file:{{diretorioSaida}}");
 		
 	}
 
