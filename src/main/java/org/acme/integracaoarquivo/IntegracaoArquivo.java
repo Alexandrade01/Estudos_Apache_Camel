73bbdff6f2baf6983c5349904f72d595db0d1870
Alexandre Bezerra de Andrade <solidwinter96@outlook.com>
2023-11-06 13:22:22 -0300

    criacao de propriedade para CNPJ

--- src/main/java/org/acme/integracaoarquivo/IntegracaoArquivo.java
+++ src/main/java/org/acme/integracaoarquivo/IntegracaoArquivo.java
@@ -1,21 +1,24 @@
 package org.acme.integracaoarquivo;
 
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.file.FileConstants;
 import org.apache.camel.component.http.HttpConstants;
+import org.apache.camel.support.builder.Namespaces;
 
 public class IntegracaoArquivo extends RouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
 		
+		var ns = new Namespaces("ns","http://www.portalfiscal.inf.br/nfe");
+		
 		from("file:{{diretorioEntrada}}?delay=5000")
 		.routeId("Integracao-Arquivo")
 		.log("Processando o arquivo: ${file:onlyname}")
+		.setProperty("CNPJ",xpath("{{xpathCnpjTransportadora}}", ns))
 		.choice()
-		.when(xpath("{{xpathCnpjTransportadora}}").isEqualTo("1"))
+		.when(exchangeProperty("CNPJ").isEqualTo("1"))
 		.to("file:{{diretorioTransportadora1}}?fileName=${date:now:HHmmss}_${file:name}")
-		.when(xpath("{{xpathCnpjTransportadora}}").isEqualTo("2"))
+		.when(exchangeProperty("CNPJ").isEqualTo("2"))
 		.setHeader(HttpConstants.HTTP_METHOD, constant("POST"))
 		.setHeader(HttpConstants.HTTP_URI, constant("{{urlApiTransportadora2}}"))
 		.setHeader(HttpConstants.HTTP_PATH, constant("nfes"))
