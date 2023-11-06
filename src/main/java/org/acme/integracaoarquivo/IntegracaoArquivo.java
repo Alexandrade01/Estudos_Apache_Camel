5e06e7d77c155b75b958e2b972954a4211404ef9
Alexandre Bezerra de Andrade <solidwinter96@outlook.com>
2023-11-06 17:36:14 -0300

    dividindo componente via direct

--- src/main/java/org/acme/integracaoarquivo/IntegracaoArquivo.java
+++ src/main/java/org/acme/integracaoarquivo/IntegracaoArquivo.java
@@ -16,22 +16,28 @@ public class IntegracaoArquivo extends RouteBuilder {
 		.log("Processando o arquivo: ${file:onlyname}")
 		.setProperty("CNPJ",xpath("{{xpathCnpjTransportadora}}", ns))
 		.choice()
-		.when(exchangeProperty("CNPJ").isEqualTo("1"))
-		.to("file:{{diretorioTransportadora1}}?fileName=${date:now:HHmmss}_${file:name}")
+		.when(exchangeProperty("CNPJ").isEqualTo("1")).to("direct:integracaoTransportadora1")
 		
-		.when(exchangeProperty("CNPJ").isEqualTo("2"))
-		// throttle limita o número de mensagens que podem passar por um ponto específico de uma rota em um determinado período de tempo.
-			.throttle(1).timePeriodMillis(5000)
-				.setHeader(HttpConstants.HTTP_METHOD, constant("POST"))
-				.setHeader(HttpConstants.HTTP_URI, constant("{{urlApiTransportadora2}}"))
-				.setHeader(HttpConstants.HTTP_PATH, constant("nfes"))
-				.setHeader(HttpConstants.CONTENT_TYPE, constant("application/xml"))
-		.to("http:servidorTransportadora2")
+		.when(exchangeProperty("CNPJ").isEqualTo("2")).to("direct:integracaoTransportadora2")
 		.endChoice()
 		
 		.otherwise()
 		.log("Transportadora não integrada ! ")
 		.end();
+		
+		from("direct:integracaoTransportadora1")
+			.routeId("integracao-arquivo-transportadora2")
+		.to("file:{{diretorioTransportadora1}}?fileName=${date:now:HHmmss}_${file:name}");
+		
+		from("direct:integracaoTransportadora2")
+			.routeId("integracao-arquivo-transportadora1")
+			// throttle limita o número de mensagens que podem passar por um ponto específico de uma rota em um determinado período de tempo.
+			.throttle(1).timePeriodMillis(5000).asyncDelayed()
+				.setHeader(HttpConstants.HTTP_METHOD, constant("POST"))
+				.setHeader(HttpConstants.HTTP_URI, constant("{{urlApiTransportadora2}}"))
+				.setHeader(HttpConstants.HTTP_PATH, constant("nfes"))
+				.setHeader(HttpConstants.CONTENT_TYPE, constant("application/xml"))
+		.to("http:servidorTransportadora2");
 	}
 
 }
